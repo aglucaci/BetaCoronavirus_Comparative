11fdae16e71c2244df50085c044a5f8bd23495ae
Alexander Lucaci <43097993+aglucaci@users.noreply.github.com>
2020-08-08 12:55:36 -0400

    Add files via upload
    send output to blastn_QA_filtered directory

--- QA_blastn.py
+++ QA_blastn.py
@@ -32,7 +32,7 @@ path = "../analysis/blastn_results"
 # =============================================================================
 # Helper function
 # =============================================================================
-def nt_content(filename):
+def nt_content(filename,path,output_dir):
     count = 0
     nt_distribution = [] # NT content distribution among the sequences
     with open(filename, "r") as handle:
@@ -52,6 +52,15 @@ def nt_content(filename):
     passed = [x for x in nt_distribution if x >= threshold]
     print("\t# Number that would pass:", len(passed))
     
+    output = os.path.join(output_dir, filename.split("/")[-1])
+    print("\t# Saving data to:", output)
+    
+    # Clear out the ouput file, create a null one
+    with open(output, "w") as handle:
+        #SeqIO.write(record, handle, "fasta")
+        handle.write("")
+    #end with
+    
     # Write out to file
     with open(filename, "r") as handle:
         for record in SeqIO.parse(handle, "fasta"): 
@@ -62,8 +71,8 @@ def nt_content(filename):
                 
                 #write to file.
                 pass
-                output = filename.replace(".fasta", "_filtered.fasta")
-                print("\t# Saving data to:", output)
+                
+                #output = filename.replace(".fasta", "_filtered.fasta")
                 with open(output, "a") as handle:
                     SeqIO.write(record, handle, "fasta")
                 #end with
@@ -73,6 +82,8 @@ def nt_content(filename):
     #end with
     return count, nt_distribution
 #end total_number_seqs
+
+
 
 # =============================================================================
 # Main subroutine
@@ -84,11 +95,15 @@ def nt_content(filename):
 for virus in ["MERS", "SARS", "SARS2"]:
     newpath = os.path.join(path, virus)
     print("## Checking:", newpath)
+    output_dir = os.path.join(newpath, "blastn_QA_filtered")
+    print("# Creating output directory:", output_dir)
+    os.system("mkdir -p " + output_dir)
     for filename in os.listdir(newpath):
         if filename.endswith(".fasta"):
             fullpath = os.path.join(path, virus, filename)
+            if "filtered" in filename: continue
             print("\t# Processing:", fullpath)
-            count, nt_distribution = nt_content(fullpath)
+            count, nt_distribution = nt_content(fullpath, path, output_dir)
             print("\t# Number of sequences (total):", count)
             print("\t#", stats.describe(nt_distribution))
             print()
