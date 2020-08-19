8da21f70767e9e79e5f604cafa02dce07ca3cb98
aglucaci <alexander.lucaci@temple.edu>
2020-08-19 10:49:13 -0400

    Init

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -2,6 +2,7 @@
 
 #@Usage: bash pipeline.sh
 
+#@REFERENCE FOR PIPELINE ON SLURM: https://hpc.nih.gov/docs/job_dependencies.html
 clear
 
 echo "## Starting pipeline"
@@ -29,15 +30,21 @@ echo "# Starting to submit jobs"
 echo "# ##################################"
 
 # Step 1a - Split out ViPR results, gene by gene for each viral species
-echo bash blastn_genebygene.sh $BASEDIR
-#bash blastn_genebygene.sh $BASEDIR
+if [ -s $BASEDIR"/scripts/SARS2_db.nto" ];
+then
+    echo "# blastn results complete"
+else
+    echo bash blastn_genebygene.sh $BASEDIR
+    bash blastn_genebygene.sh $BASEDIR
+fi
+
 echo ""
 
 # Step 1b - QA on blastn, this filters the results of blastn search that have less than 99% NT content for that gene
 # meaning, it filters out sequences that are less than 99% of the sequence length
 if [ -s $BASEDIR"/analysis/blastn_results/SARS2/blastn_QA_filtered/ORF10.fasta" ];
 then
-    echo 1 
+    #echo 1 
     echo "# QA on blastn results is complete" 
 else
     echo python36 QA_blastn.py $BASEDIR > $BASEDIR"/scripts/QA_blastn_log.txt"
@@ -52,10 +59,12 @@ jobid_2a=$($cmd | cut -d' ' -f3)
 echo "Step 2a: "$jobid_2a
 echo ""
 
+#exit 1
+# Some kind of check needs to be implemented here, to make sure that all of the alignments were generated
 
 # Step 2b - ModelTest
-echo qsub -V -q epyc -W depend=afterok:$jobid_2a run_modeltest.sh
-cmd="qsub -V -q epyc -W depend=afterok:$jobid_2a run_modeltest.sh"
+echo qsub -V -q epyc -o $BASEDIR"/scripts/STDIN" -e $BASEDIR"/scripts/STDIN_Error" -W depend=afterok:$jobid_2a run_modeltest.sh
+cmd="qsub -V -q epyc -o $BASEDIR"/scripts/STDIN" -e $BASEDIR"/scripts/STDIN_Error" -W depend=afterok:$jobid_2a run_modeltest.sh"
 jobid_2b=$($cmd | cut -d' ' -f3)
 echo "Step 2b: "$jobid_2b
 echo ""
@@ -68,30 +77,33 @@ jobid_2c=$($cmd | cut -d' ' -f3)
 echo "Step 2c: "$jobid_2c
 echo ""
 
+#exit 2
+# Some kind of check needs to be implemented here, to make sure that all of the trees were generated.
+
 # Step 3a - Perform a TN93 distance calculation (on codon aware msa)
-echo qsub -V -W depend=afterok:$jobid_2a -q epyc -N tn93_codons tn93_codons.sh -F $BASEDIR
-cmd="qsub -V -W depend=afterok:$jobid_2a -q epyc -N tn93_codons tn93_codons.sh -F $BASEDIR"
+echo qsub -V -W depend=afterok:$jobid_2a -q epyc -o $BASEDIR"/scripts/STDIN" -e $BASEDIR"/scripts/STDIN_Error" -N Beta_tn93_codons tn93_codons.sh -F $BASEDIR
+cmd="qsub -V -W depend=afterok:$jobid_2a -q epyc -o $BASEDIR"/scripts/STDIN" -e $BASEDIR"/scripts/STDIN_Error" -N Beta_tn93_codons tn93_codons.sh -F $BASEDIR"
 jobid_3a=$($cmd | cut -d' ' -f3)
 echo "Step 3a: "$jobid_3a
 echo ""
 
 # Step 3b - TN93 Cluster analysis
-echo qsub -V -W depend=afterok:$jobid_2a -q epyc tn93_cluster.sh -F $BASEDIR
-cmd="qsub -V -W depend=afterok:$jobid_2a -q epyc tn93_cluster.sh -F $BASEDIR"
+echo qsub -V -W depend=afterok:$jobid_2a -o $BASEDIR"/scripts/STDIN" -e $BASEDIR"/scripts/STDIN_Error" -q epyc tn93_cluster.sh -F $BASEDIR
+cmd="qsub -V -W depend=afterok:$jobid_2a -o $BASEDIR"/scripts/STDIN" -e $BASEDIR"/scripts/STDIN_Error" -q epyc tn93_cluster.sh -F $BASEDIR"
 jobid_3b=$($cmd | cut -d' ' -f3)
 echo "Step 3b: "$jobid_3b
 echo ""
 
 # Step 4 - Selection Analyses
-echo qsub -V -W depend=afterok:$jobid_2c -q epyc -l nodes=1:ppn=28 MEME_BUSTEDS_aBSREL.sh -F $BASEDIR
-cmd="qsub -V -W depend=afterok:$jobid_2c -q epyc -l nodes=1:ppn=28 MEME_BUSTEDS_aBSREL.sh -F $BASEDIR"
+echo qsub -V -W depend=afterok:$jobid_2c -o $BASEDIR"/scripts/STDIN" -e $BASEDIR"/scripts/STDIN_Error" -q epyc -l nodes=1:ppn=28 MEME_BUSTEDS_aBSREL.sh -F $BASEDIR
+cmd="qsub -V -W depend=afterok:$jobid_2c -o $BASEDIR"/scripts/STDIN" -e $BASEDIR"/scripts/STDIN_Error" -q epyc -l nodes=1:ppn=28 MEME_BUSTEDS_aBSREL.sh -F $BASEDIR"
 jobid_4=$($cmd | cut -d' ' -f3)
 echo "Step 4: "$jobid_4
 echo ""
 
 # Step 5 - use MAFFT Merge to merge the alignments across genes, and then construct ML Trees.
-echo qsub -V -W depend=afterok:$jobid_2a -q epyc MergeAlignments.sh -F $BASEDIR
-cmd="qsub -V -W depend=afterok:$jobid_2a -q epyc MergeAlignments.sh -F $BASEDIR"
+echo qsub -V -W depend=afterok:$jobid_2a -o $BASEDIR"/scripts/STDIN" -e $BASEDIR"/scripts/STDIN_Error" -q epyc MergeAlignments.sh -F $BASEDIR
+cmd="qsub -V -W depend=afterok:$jobid_2a -o $BASEDIR"/scripts/STDIN" -e $BASEDIR"/scripts/STDIN_Error" -q epyc MergeAlignments.sh -F $BASEDIR"
 jobid_5=$($cmd | cut -d' ' -f3)
 echo "Step 5: "$jobid_5
 echo ""
@@ -101,15 +113,15 @@ echo ""
 # Step 6 - Partition lineages
 #echo bash launcher_for_annotator.sh -F $BASEDIR 
 #bash launcher_for_annotator.sh -F $BASEDIR 
-echo qsub -V -W depend=afterok:$jobid_5 -q epyc launcher_for_annotator.sh -F $BASEDIR 
-cmd="qsub -V -W depend=afterok:$jobid_5 -q epyc launcher_for_annotator.sh -F $BASEDIR"
+echo qsub -V -W depend=afterok:$jobid_5 -o $BASEDIR"/scripts/STDIN" -e $BASEDIR"/scripts/STDIN_Error" -q epyc launcher_for_annotator.sh -F $BASEDIR 
+cmd="qsub -V -W depend=afterok:$jobid_5 -o $BASEDIR"/scripts/STDIN" -e $BASEDIR"/scripts/STDIN_Error" -q epyc launcher_for_annotator.sh -F $BASEDIR"
 jobid_6=$($cmd | cut -d' ' -f3)
 echo "Step 6: "$jobid_6
 echo ""
 
 # Step 7 - Run RELAX and Constrast-FEL
-echo qsub -V -W depend=afterok:$jobid_5 -q epyc RELAX_CONTRAST-FEL.sh -F $BASEDIR
-cmd="qsub -V -W depend=afterok:$jobid_5 -q epyc RELAX_CONTRAST-FEL.sh -F $BASEDIR"
+echo qsub -V -W depend=afterok:$jobid_6 -o $BASEDIR"/scripts/STDIN" -e $BASEDIR"/scripts/STDIN_Error" -q epyc RELAX_CONTRAST-FEL.sh -F $BASEDIR
+cmd="qsub -V -W depend=afterok:$jobid_6 -o $BASEDIR"/scripts/STDIN" -e $BASEDIR"/scripts/STDIN_Error" -q epyc RELAX_CONTRAST-FEL.sh -F $BASEDIR"
 jobid_7=$($cmd | cut -d' ' -f3)
 echo "Step 7: "$jobid_7
 
